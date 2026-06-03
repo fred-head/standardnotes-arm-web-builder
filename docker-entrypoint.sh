c1bc2aac80ad89823be3022843326daa285aed65
fred-head <68735922+fred-head@users.noreply.github.com>
2026-06-03 20:18:17 +0200

    Add sync server configuration to entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
 
+set -e
+
+DEFAULT_SYNC_SERVER="${DEFAULT_SYNC_SERVER:-https://api.standardnotes.com}"
+
+echo "Using sync server: ${DEFAULT_SYNC_SERVER}"
+
+sed -i \
+  "s|__DEFAULT_SYNC_SERVER__|${DEFAULT_SYNC_SERVER}|g" \
+  /usr/share/nginx/html/index.html
+
+exec nginx -g "daemon off;"
